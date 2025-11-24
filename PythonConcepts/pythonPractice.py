8f5f47d48750be8e04e2557c07f5161e25d2bc4b
Surya Teja Chatrathi <chatrathisuryateja6@gmail.com>
2025-11-24 07:33:47 +0530

    PythonPatterns

--- PythonConcepts/pythonPractice.py
+++ PythonConcepts/pythonPractice.py
@@ -1,15 +1,14 @@
 '''
-* 
-* * 
-* * *
-* * * *
-* * * * *
-* * * * * *
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
 '''
 
 N = 5
-
 for i in range(N):
     for j in range(i+1):
-        print("*",end="")
+        print(j+1,end=" ")
     print()
