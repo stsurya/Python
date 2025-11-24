703b073a5a087dd3f62f803a738fa675a3aabc7a
Surya Teja Chatrathi <chatrathisuryateja6@gmail.com>
2025-11-24 11:51:04 +0530

    PythonPatterns

--- DSA/pythonPatterns.md
+++ DSA/pythonPatterns.md
@@ -155,3 +155,22 @@ for i in range(N):
         print(" ",end=" ")
     print()
 ```
+
+```
+*  
+**
+***  
+**
+*    
+
+N = 6
+for i in range(N):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+for i in range(N,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+```
--- PythonConcepts/pythonPractice.py
+++ PythonConcepts/pythonPractice.py
@@ -1,27 +1,18 @@
 '''
-  *  
- ***
-***** 
-*****  
- ***
-  *    
+*  
+**
+***  
+**
+*    
 '''
 
-N = 3
-for i in range(N):
-    for j in range(N-i):
-        print(" ",end=" ")
-    for j in range(2*i+1):
-        print("*",end=" ")
-    for j in range(i):
-        print(" ",end="")
-    print()
-
+N = 6
 for i in range(N):
     for j in range(i+1):
-        print(" ",end=" ")
-    for j in range(2*N-(2*i+1)):
         print("*",end=" ")
+    print()
+
+for i in range(N,0,-1):
     for j in range(i):
-        print(" ",end=" ")
+        print("*",end=" ")
     print()
